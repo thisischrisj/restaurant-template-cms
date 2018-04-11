29b59d94b4335b9c0077f125540aec7b9d732957
Christopher Jones <dev@thecodery.co.uk>
2018-04-11 14:57:49 +0100

    Forms

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,17 @@
 // JS Goes here - ES6 supported
-const mySiema = new Siema({
-  selector: '.gallery-carousel',
-  duration: 200,
-  easing: 'ease',
-  perPage: 1,
-  startIndex: 0,
-  draggable: true,
-  threshold: 20,
-  loop: true
-});
+if(document.querySelector('.gallery-carousel'))
+{
+	const mySiema = new Siema({
+	  selector: '.gallery-carousel',
+	  duration: 200,
+	  easing: 'ease',
+	  perPage: 1,
+	  startIndex: 0,
+	  draggable: true,
+	  threshold: 20,
+	  loop: true
+	});
 
-document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
-document.querySelector('.next').addEventListener('click', () => mySiema.next());
+	document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
+	document.querySelector('.next').addEventListener('click', () => mySiema.next());
+}
